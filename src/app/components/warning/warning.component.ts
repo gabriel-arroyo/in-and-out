69052f222ab43053cf7be775beba2f94d9c02792
gabriel-arroyo <ing.gabriel.arroyo@gmail.com>
2022-04-24 17:49:10 -0500

    animations

--- src/app/components/warning/warning.component.ts
+++ src/app/components/warning/warning.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos'
 
 @Component({
   selector: 'app-warning',
@@ -10,6 +11,7 @@ export class WarningComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init()
   }
 
 }
