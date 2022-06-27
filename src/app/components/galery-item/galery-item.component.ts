33c5b61a36db61c208a9fcde5ca11a5cfd184dfd
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-06-27 08:42:35 -0500

    black background

--- src/app/components/galery-item/galery-item.component.ts
+++ src/app/components/galery-item/galery-item.component.ts
@@ -8,9 +8,29 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GaleryItemComponent implements OnInit {
   @Input() model = '';
   @Input() price = '';
+  @Input() src = '../../../assets/images/img1.png';
+  @Input() index = '0';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isEven(str: string) {
+    let n = parseInt(str)
+    let even = n % 2 === 0
+    return even
+  }
+
+  isLeft(str: string) {
+    let n = parseInt(str)
+    let even = n % 3 === 0
+    return even
+  }
+
+  isRight(str: string) {
+    let n = parseInt(str)
+    let even = ((n + 1) % 3) === 0
+    return even
   }
 
 }
